b0b814a0f36b652ee0ffb1aa7b7d5ea71c0fa224
manzitresor <manzitre@gmail.com>
2024-09-04 22:37:23 +0200

    chore: use streams to handle large on Get request

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
+
     if(url === '/' && method === 'GET') {
-        fs.readFile('./input.txt','utf-8',(error, data) => {
-            if(data) {
-                res.setHeader('Content-type','text/plain');
-                res.statusCode =  200;
-                res.write(data);
-                res.end();
-            } else {
-                res.statusCode = 400;
-                res.write(error);
-                res.end();
-            }
+        const readStream = fs.createReadStream('./input.txt','utf-8');
+
+        readStream.on('error',(error) => {
+            res.statusCode = 404;
+            res.write(error);
+            res.end();
         })
+
+        res.setHeader('Content-type','text/plain');
+        res.statusCode =  200;
+        readStream.pipe(res)
     }
     if(url === '/message' && method === 'POST'){
         const body = [];
